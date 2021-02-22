3aa154367126e4dbc4d5d36e5b5c4fe5017ec7d9
uidoyen <nasiba.ansari@gmail.com>
2021-02-22 16:47:33 +0530

    updating Jest config

--- ui/packages/ui/jest.config.js
+++ ui/packages/ui/jest.config.js
@@ -1,23 +1,12 @@
 module.exports = {
-    roots: [
-        "<rootDir>/src",
-        "<rootDir>/__tests__"
-    ],
-    transform: {
-        "^.+\\.ts$": "ts-jest"
-    },
-    testRegex: "(/__tests__/.*.(test|spec)).(jsx?|tsx?|js)$",
-    moduleFileExtensions: [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
-    collectCoverage: true,
-    coveragePathIgnorePatterns: [
-        "(__tests__/.*.mock).(jsx?|tsx?)$"
-    ],
-    verbose: true
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "identity-obj-proxy"
+  },
+  "moduleDirectories": ["node_modules"],
+  setupFilesAfterEnv: [
+    "<rootDir>/setupEnzyme.ts"
+  ],
 };
