e2ccfe1334e56672c11961ba3c0edbbd30a428b7
uidoyen <nasiba.ansari@gmail.com>
2021-02-22 19:01:00 +0530

    adding yarn lint

--- ui/packages/ui/jest.config.js
+++ ui/packages/ui/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   "moduleNameMapper": {
     "\\.(css|less|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy"
   },
-  "moduleDirectories": ["node_modules"],
+  "moduleDirectories": ["node_modules", "src"],
   setupFilesAfterEnv: [
     "<rootDir>/setupEnzyme.ts"
   ],
